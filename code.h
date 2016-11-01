8d02f9e53cf8b9a3ef0b09c2c5fe66cf0f477591
Tagline Treichel <tagline.treichel@gmail.com>
2016-11-01 00:16:08 -0200

    Etapa 5 - base pronta

--- code.h
+++ code.h
@@ -7,8 +7,3 @@
 
 TAC* generateCode(ASTREE *node);
 
-//TAC* makeIf(TAC* cond, TAC* then, TAC* els);
-//TAC* makeWhile(TAC* expr, TAC* cmd);
-//TAC* funcDecl(HASH_NODE* symbol, TAC* type, TAC* params, TAC* cmds);
-//TAC* makeAtrVec(HASH_NODE* symbol, TAC* exprIndex, TAC* cmd);
-
